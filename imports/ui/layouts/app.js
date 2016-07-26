b6348c674f2574292b35152fff3ca39b4434ccbb
Paul Savignano <Paul.Savignano@gmail.com>
2016-07-26 11:29:28 -0700

    Bump Meteor version and NPM packages.

--- imports/ui/layouts/app.js
+++ imports/ui/layouts/app.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Grid } from 'react-bootstrap'
 import AppNavigation from '../containers/app-navigation'
 
-export class App extends Component {
+export const App = React.createClass({
+  propTypes: {
+    children: React.PropTypes.element.isRequired,
+  },
   render() {
-    return (
-      <div>
-        <AppNavigation />
-        <Grid>
-          { this.props.children }
-        </Grid>
-      </div>
-    )
-  }
-}
-
-App.propTypes = {
-  children: React.PropTypes.element.isRequired,
-}
+    return <div>
+      <AppNavigation />
+      <Grid>
+        { this.props.children }
+      </Grid>
+    </div>
+  },
+})
